698d3aabe1b69afbb76fe2ca27f02851cb0f0e7d
鎌形 竜也 <x21j030@chiba-fjb.ac.jp>
2021-09-21 17:01:45 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/chibafjb/tsuyokikao/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.tsuyokikao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
